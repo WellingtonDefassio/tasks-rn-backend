c7dc81c3518bfda058ebbb994aef80f929d0326d
WellDefassio <well.defassio@gmail.com>
2023-11-26 20:20:20 -0300

    add spring security

--- src/main/java/wdefassio/io/tasksbackend/api/controllers/HelloController.java
+++ src/main/java/wdefassio/io/tasksbackend/api/controllers/HelloController.java
@@ -1,15 +1,20 @@
-package wdefassio.io.tasksbackend.controllers;
+package wdefassio.io.tasksbackend.api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping()
 public class HelloController {
 
     @GetMapping
     public String helloController() {
+        return "Hello Controller";
+    }
+
+    @GetMapping("/Hello")
+    public String helloController2() {
         return "Hello Controller";
     }
 
---
+++ src/main/java/wdefassio/io/tasksbackend/api/controllers/SignInController.java
@@ -0,0 +1,7 @@
+package wdefassio.io.tasksbackend.api.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SignInController {
+}
