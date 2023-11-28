4fd966851473f03ba972484b7ad3588d0441b4c0
WellingtonDefassio <well.defassio@gmail.com>
2023-11-27 23:03:32 -0300

    refactor controller

--- src/main/java/wdefassio/io/tasksbackend/api/HelloController.java
+++ src/main/java/wdefassio/io/tasksbackend/api/HelloController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping()
 @RequiredArgsConstructor
 public class HelloController {
     @GetMapping("/hello")
-    public String helloController() {
-        return "Hello";
+    public String helloController(Principal principal) {
+        return "Hello " + principal.getName();
     }
 
 }
